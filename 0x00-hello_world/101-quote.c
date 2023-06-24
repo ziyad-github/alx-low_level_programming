acf64c9136224f8e684f5300a5dd9bfe751100b7
ziyad-github <ziyadqsr@gmail.com>
2023-06-24 06:52:32 -0700

    Light fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - this function prints a text
+ * Return: 1 on success, non-one otherwise
+*/
+int main(void) {
+const char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t len = strlen(str);
+write(2, str, len);
+return (1);
+}
