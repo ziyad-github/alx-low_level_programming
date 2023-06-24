d000ddb1145a4ebda6ecd92aba9921796d609eb2
ziyad-github <ziyadqsr@gmail.com>
2023-06-24 05:39:19 -0700

    Light fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * This function simply prints a string.
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
