027c6ef31382fdf5e8202c4e3f2616f82f31bcec
ziyad-github <ziyadqsr@gmail.com>
2023-06-24 06:58:39 -0700

    Light fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  * Return: 1 on success, non-one otherwise
 */
 int main(void) {
-const char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-size_t len = strlen(str);
-write(2, str, len);
+const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = sizeof(str) - 1;
+write(STDERR_FILENO, str, len);
 return (1);
 }
