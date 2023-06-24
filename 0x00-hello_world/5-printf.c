78f8d970cc2910d8249cdc28fd16049ec0611590
ziyad-github <ziyadqsr@gmail.com>
2023-06-24 05:48:20 -0700

    Light fix

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * Return: 0 on success, non-zero otherwise
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
