71fdfe7715492440c0dd12f28900b8ca6ded7c94
ziyad-github <ziyadqsr@gmail.com>
2023-06-25 09:16:30 -0700

    Light fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints numbers
+ * Return: 0 on success, non-zero otherwise
+*/
+int main(void)
+{
+int i;
+  
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+}
