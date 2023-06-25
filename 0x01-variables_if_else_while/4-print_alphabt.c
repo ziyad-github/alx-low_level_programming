b4fc0b46b959b039b2706c2a71dccc6c373ec0a1
ziyad-github <ziyadqsr@gmail.com>
2023-06-25 08:51:18 -0700

    Light fix

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints alphabets
+ * Return: 0 on success, non-zero otherwise
+ */
+int main(void)
+{
+const char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+if (i == 4 or i == 15)continue;
+putchar(alphabets[i]);
+}
+putchar('\n');
+return (0);
+}
