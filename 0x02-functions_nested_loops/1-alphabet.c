adeda14959fce656e956464cedac8996a1bae300
ziyad-github <ziyadqsr@gmail.com>
2023-06-26 04:49:15 -0700

    Light fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,17 @@
 
 /**
  * print_alphabet - prints all the alphabets
+ *
  * Return: void
  */
 void print_alphabet(void)
 {
-  const char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
-  int i;
+    const char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
 
-  for (i = 0; i < 26; i++)
-  {
-    _putchar(alphabets[i]);
-  }
-  _putchar('\n');
+    for (i = 0; i < 26; i++)
+    {
+        _putchar(alphabets[i]);
+    }
+    _putchar('\n');
 }
