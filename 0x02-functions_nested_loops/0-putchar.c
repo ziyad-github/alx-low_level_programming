46ad24bef3f37f22c62e8ac50df00c7b591939cd
ziyad-github <ziyadqsr@gmail.com>
2023-06-26 04:00:50 -0700

    Light fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * main - prints a text
- * Return: 0 on succes, non-zero otherwise
+ * main -  prints _putchar, followed by a new line
+ * Return: 0 (on Success)
  */
 int main(void)
 {
@@ -14,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
-        return (0);
+
+	return (0);
 }
