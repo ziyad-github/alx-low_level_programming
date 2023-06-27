01c1322bded6b5ca0819887f7d035f1bcd27141e
ziyad-github <ziyadqsr@gmail.com>
2023-06-27 10:30:23 -0700

    Light fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <ctype.h>
+
 /**                                                                                                                                         
  * _islower - checks if a char is in lower case or not                                                                                      
  * Return: 1 on true, 0 on false                                                                                                            
  */
-
 int _islower(int c)
 {
-	return (int) islower(c);
+	if (islower(c))
+		return 1;
+	else
+		return 0;
 }
