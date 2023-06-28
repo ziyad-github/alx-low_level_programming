59cc7a0e9171062052164306bd03d1949e6e4b44
ziyad-github <ziyadqsr@gmail.com>
2023-06-27 19:13:59 -0700

    Light fix

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - it prints the sign of the number added
+ * @n: the number to check
+ * Return: 1 on positive, 0 on zero, -1 on negative
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	      	_putchar('+');
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}
