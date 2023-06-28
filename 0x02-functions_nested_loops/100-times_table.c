e66040f70e2a6c95111c5ec327f4798eb4e2d0a0
ziyad-github <ziyadqsr@gmail.com>
2023-06-28 16:30:38 -0700

    Light fix

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the tables of n
+ * @n: the number to give off the table
+ */
+void print_times_table(int n)
+{
+        int i, j, k;
+
+	if (n < 0 || n > 15)
+	   return;
+
+        for (i = 0; i < n; i++)
+        {
+                for (j = 0; j < n; j++)
+                {
+                        k = j * i;
+                        if (j == 0)
+                        {
+                                _putchar(k + '0');
+                        }
+
+                        if (k < n && j != 0)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(k + '0');
+                        } else if (k >= n)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar((k / 10) + '0');
+                                _putchar((k % 10) + '0');
+                        }
+                }
+                _putchar('\n');
+        }
+}
