2e47487506f469e1b10347b93efb65b56a8f5bf3
ziyad-github <ziyadqsr@gmail.com>
2023-06-28 16:32:28 -0700

    Light fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 	   return;
 
-        for (i = 0; i < n; i++)
+        for (i = 0; i <= n; i++)
         {
-                for (j = 0; j < n; j++)
+                for (j = 0; j <= n; j++)
                 {
                         k = j * i;
                         if (j == 0)
