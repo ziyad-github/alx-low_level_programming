00d43ca6fcb0d679f8f496016476780c8d546c91
ziyad-github <ziyadqsr@gmail.com>
2023-06-28 16:52:04 -0700

    Light fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,11 +26,13 @@ void print_times_table(int n)
                                 _putchar(',');
                                 _putchar(' ');
                                 _putchar(' ');
+				_putchar(' ');
                                 _putchar(k + '0');
                         } else if (k >= 10)
                         {
                                 _putchar(',');
                                 _putchar(' ');
+				_putchar(' ');
                                 _putchar((k / 10) + '0');
                                 _putchar((k % 10) + '0');
                         }
