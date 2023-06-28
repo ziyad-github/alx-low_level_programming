0d49b4be6a17a4f5617722d607a3a1e6996e9cf7
ziyad-github <ziyadqsr@gmail.com>
2023-06-28 07:40:00 -0700

    Light fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - it prints the table of 9
+ * Return: void
+*/
+void times_table(void)
+{
+	int horizental, vertical, result;
+
+	for (horizental = 0; horizental < 9; horizental++)
+	{
+		for (vertical = 0; vertical < 9; vertical++)
+		{
+			result = horizental * vertical;
+			if (result > 9)
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+			{
+				_putchar('0' + result);
+			}
+
+			if (vertical != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
