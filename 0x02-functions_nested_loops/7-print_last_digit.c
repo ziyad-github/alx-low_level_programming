412b9b8a18338b76245d1dad9b385959562aca41
ziyad-github <ziyadqsr@gmail.com>
2023-06-27 19:57:44 -0700

    Light fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * print_last_digit - it prints the last digit of a number
@@ -7,5 +8,5 @@
 */
 int print_last_digit(int n)
 {
-	return (_abs(n) % 10);
+	return (abs(n) % 10);
 }
