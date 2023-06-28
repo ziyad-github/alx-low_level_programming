0b369636da7de86e88c0ca6329d87b4ad92e66e9
ziyad-github <ziyadqsr@gmail.com>
2023-06-28 09:09:04 -0700

    Light fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,21 +4,20 @@
  * print_alphabet_x10 - Entry point
  * Return:0
  */
-
 void print_alphabet_x10(void)
 {
-  char c = 'a';
-  int i = 0;
+	char c = 'a';
+	int i = 0;
 
-  while (i < 10)
-  {
-    while (c <= 'z')
-    {
-      _putchar(c);
-      c++;
-    }
-    c = 'a';
-    i++;
-    _putchar('\n');
-  }
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		c = 'a';
+		i++;
+		_putchar('\n');
+	}
 }
