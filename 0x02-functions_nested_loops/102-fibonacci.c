69df85755eb683d6fabaca9da8a5d4edc3c15cd6
ziyad-github <ziyadqsr@gmail.com>
2023-06-28 18:09:20 -0700

    Light fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	int n1 = 1;
-	int n2 = 2;
-	int next_n = 0;
+	long int n1 = 1;
+	long int n2 = 2;
+	long int next_n = 0;
 	int i;
 
 	for (i = 1; i < 50; i++)
@@ -18,6 +18,6 @@ int main(void)
 		n1 = n2;
 		n2 = next_n;
 	}
-
+	putchar('\n');
 	return (0);
 }
