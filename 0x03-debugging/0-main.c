ae69d9ab108c4bc9992e7e1f34f8693522570dfb
ziyad-github <ziyadqsr@gmail.com>
2023-06-30 05:12:53 -0700

    Light fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - test the function pos or neg
- * Return: 0 on success, non-zero otherwise
- */
- 
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
