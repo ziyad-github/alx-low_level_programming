d2ee7779b0f70c062c75aa09c28531a439bf7e92
ziyad-github <ziyadqsr@gmail.com>
2023-06-30 05:04:20 -0700

    Light fix

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test the function pos or neg
+ * Return: 0 on success, non-zero otherwise
+ */
+ 
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
