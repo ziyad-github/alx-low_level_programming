432b07c71e9edbf5672f674e5d2111662551178d
ziyad-github <ziyadqsr@gmail.com>
2023-07-02 09:56:03 -0700

    Light fix

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiply two numbers
+* @a: the digit to multiply
+* @b: the other digit to multiply 
+* Return: 1 on upper and 0 otherwise
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
