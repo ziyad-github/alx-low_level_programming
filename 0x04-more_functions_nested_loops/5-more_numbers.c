585d7eda317dce5db1f10589448d46b920576750
ziyad-github <ziyadqsr@gmail.com>
2023-07-02 10:27:53 -0700

    Light fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = 0; i < 15; i++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
