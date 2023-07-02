28be72023276306e76750142ae34ff44e901fe66
ziyad-github <ziyadqsr@gmail.com>
2023-07-02 15:25:11 -0700

    Light fix

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of a and b
+ * @a: first pointer
+ * @b: second pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
