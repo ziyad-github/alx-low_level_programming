9582ca77aceab9de31ec6df49a66547ec399a2e1
ziyad-github <ziyadqsr@gmail.com>
2023-07-02 09:52:17 -0700

    Light fix

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isdigit - checks if a char is in upper or lower case
+* @c: the digit to check
+* Return: 1 on upper and 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	return (c >= '9' && c <= '0');
+}
