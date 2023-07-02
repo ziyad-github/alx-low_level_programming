f9ac2097ffb47f0a178334663e624bb62ebdfef3
ziyad-github <ziyadqsr@gmail.com>
2023-07-02 10:22:54 -0700

    Light fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* print_numbers - print numbers from 0 to 9
+* more_numbers - print numbers from 0 to 9
 * Return: void
 */
 
-void print_numbers(void)
+void more_numbers(void)
 {
 	int i, j;
 
