0daeba30a7b99b9d117dd79fad3fdfb7e65e838e
ziyad-github <ziyadqsr@gmail.com>
2023-07-03 08:29:22 -0700

    Light fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2: second string
  * Return: 0 on equal, and difference (-) of strings otherwise
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
