ee29522b98a26643db28292e6faaf62e8503ddf4
ziyad-github <ziyadqsr@gmail.com>
2023-07-03 09:54:27 -0700

    Light fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes string in 1337
+ * @s: string to be encoded
+ * Return: string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
