7af5c87d36710af7b10f8117af04a0b451a71017
ziyad-github <ziyadqsr@gmail.com>
2023-07-03 04:56:44 -0700

    Light fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,20 +15,13 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 
 	while (dest[i] != '\0')
-	      i++;
+		i++;
 
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
-		if (j <= n)
-		{
-			dest[i] = src[j];
-			j++;
-			i++;
-		}
-		else
-		{
-			break;
-		}
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
 	dest[i] = '\0';
