8da949c947d43e22900121c4a2b873559261fce3
ziyad-github <ziyadqsr@gmail.com>
2023-09-05 15:47:46 -0700

    Quick modification - (!automatic push)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _puts_recursion - this function print string using recursion
@@ -7,7 +7,7 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s !== '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
