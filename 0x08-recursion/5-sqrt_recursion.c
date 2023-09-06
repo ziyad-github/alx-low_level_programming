0201b2856768235c30e0fd6e615d35342a813da2
ziyad-github <ziyadqsr@gmail.com>
2023-09-05 17:54:52 -0700

    Quick modification - (!automatic push)

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - this function print square root of n
+ * @n: the number
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n  == 1)
+	{
+		return (1);
+	}
+	return (_sqrt(n, n));
+}
+
+/**
+ * _sqrt - this is a helper function finds square root of n
+ * @n: the number
+ * @x: approximation of square
+ * Return: square root of n
+ */
+int _sqrt(int n, int x)
+{
+	int next_x = (x + n / x) / 2;
+
+	if (next_x == x)
+	{
+		return (x);
+	}
+	return (_sqrt(n, next_x));
+}
