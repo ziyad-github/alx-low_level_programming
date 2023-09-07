b54f6e821d82f9b3ec43b91b8ca18fc9d1c627ae
ziyad-github <ziyadqsr@gmail.com>
2023-09-06 17:48:35 -0700

    Quick modification - (!automatic push)

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * is_prime_number - this function shows if a number is prime or not
+ * @n: the number
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1 ||( n % 2 == 0 && n != 2))
+	{
+		return (0);
+	}
+	else
+	{
+		return (_prime(n, 3));
+	}
+}
+
+/**
+ * _prime - this is a helper function to get divisors for a number
+ * @n: the number
+ * @x: divisor
+ * Return: 1 if prime, 0 if not
+ */
+int _prime(int n, int x)
+{
+	if (n % x == 0)
+	{
+		return (0);
+	}
+	else if (n / 2 > x)
+	{
+		return _prime(n, x + 2);
+	}
+	else
+	{
+		return (1);
+	}
+}
